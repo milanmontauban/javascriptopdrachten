57d0cf1b86dbbb27cf6981c8f4c272f3d07afbe3
Milan <32805@ma-web.nl>
2022-01-25 11:00:16 +0100

    first commit

--- opdracht 5 silver bullet/script.js
+++ opdracht 5 silver bullet/script.js
@@ -1,76 +1,165 @@
-const myTitle = document.getElementById("myTitle");
-const myImage = document.getElementById("myImage");
-const myInput = document.getElementById("myInput");
+const mytitle = document.getElementById("mytitle");
+const myimage = document.getElementById("myimage");
+const myinput = document.getElementById("myinput");
 
-let lokaties =
-    [
-        {
-            "titel": "plaats 0",
-            "img": "img/0.jpg"
-        },
-        {
-            "titel": "plaats 1",
-            "img": "img/1.jpg"
-        },
-        {
-            "titel": "plaats 2",
-            "img": "img/2.jpg"
-        },
-        ,
-        {
-            "titel": "plaats 3",
-            "img": "img/3.jpg"
-        },
-        ,
-        {
-            "titel": "plaats 4",
-            "img": "img/4.jpg"
-        },
-        ,
-        {
-            "titel": "plaats 5",
-            "img": "img/5.jpg"
-        },
-        ,
-        {
-            "titel": "plaats 6",
-            "img": "img/6.jpg"
-        },
-        ,
-        {
-            "titel": "plaats 7",
-            "img": "img/7.jpg"
-        },
-        ,
-        {
-            "titel": "plaats 8",
-            "img": "img/8.jpg"
-        },
-        ,
-        {
-            "titel": "plaats 9",
-            "img": "img/9.jpg"
-        },
-        ,
-        {
-            "titel": "plaats 10",
-            "img": "img/10.jpg"
-        },
-        ,
-        {
-            "titel": "plaats 11",
-            "img": "img/11.jpg"
-        },
-    ];
+let directionButtons = {
+    "noord": document.getElementById('knopNoord'),
+    "oost": document.getElementById('knopOost'),
+    "zuid":  document.getElementById('knopZuid'),
+    "west": document.getElementById('knopWest')
+}
+
+let current_index = 0;
+
+let lokaties =[ // array
+    { // 0 = object
+        "titel":"plaats 0",
+        "image":"img/0.jpg",
+        "directions": {
+            "zuid": 1
+        }
+    },
+    {  //1 = object 
+        "titel":"plaats 1",
+        "image":"img/1.jpg",
+        "directions": { // object
+            "noord": 0,
+            "west": 2,
+            "oost": 5,
+            "zuid": 4
+        }
+
+    },
+    {
+        "titel":"plaats 2",
+        "image":"img/2.jpg",
+        "directions": { // object
+            "oost": 1,
+            "zuid": 3
+        }
+    },
+    {
+        "titel":"plaats 3",
+        "image":"img/3.jpg",
+        "directions": { // object
+            "noord": 2
+            
+        }
+    },
+    {
+        "titel":"plaats 4",
+        "image":"img/4.jpg",
+        "directions": { // object
+            "noord": 1
+            
+        }
+    },
+    {
+        "titel":"plaats 5",
+        "image":"img/5.jpg",
+        "directions": { // object
+            "west": 1,
+            "oost": 6,
+            "noord": 10
+        }
+    },
+    {
+        "titel":"plaats 6",
+        "image":"img/6.jpg",
+        "directions": { // object
+            "west": 5,
+            "oost": 7,
+            "zuid": 11
+        }
+    },
+    {
+        "titel":"plaats 7",
+        "image":"img/7.jpg",
+        "directions": { // object
+            "west": 6,
+            "noord": 9,
+            "zuid": 8
+        }
+    },
+    {
+        "titel":"plaats 8",
+        "image":"img/8.jpg",
+        "directions": { // object
+            "noord": 7
+        }
+    },
+    {
+        "titel":"plaats 9",
+        "image":"img/9.jpg",
+        "directions": { // object
+            "west": 10,
+            "zuid": 7
+        }
+    },
+    {
+        "titel":"plaats 10",
+        "image":"img/10.jpg",
+        "directions": { // object
+            "oost": 9,
+            "zuid": 5
+        }
+    },
+    {
+        "titel":"plaats 11",
+        "image":"img/11.jpg",
+        "directions": { // object
+            "noord": 6
+        }
+    }
+
+]
+
+
+
+//mytitle.innerHTML = "dit is gevoegd door java";
+//myimage.src = "img/1.jpg";
 
 function show(index){
-    myTitle.innerHTML = lokaties[index].titel;
-    myImage.src = lokaties[index].img;
+    mytitle.innerHTML = lokaties[index].titel;
+    myimage.src = lokaties[index].image;
+    current_index = index;
+
+    //knoppen opnieuw berekenen
+    updateDirections();
 }
 
-function getInput(){
-    show(myInput.value);
-    //console.log(myInput.value)
-    myInput.value = "";
-    myInput.focus();
+function updateDirections(){
+
+    let possible = lokaties[current_index].directions;
+
+    let possible_keys = Object.keys(possible);
+
+    console.log(possible);
+    console.log(possible_keys);
+
+    let button_keys = Object.keys(directionButtons);
+    console.log(button_keys);
+
+    for(const key of button_keys){
+        directionButtons[key].style.visibility = "hidden";
+    }
+
+    for(const key of possible_keys){
+        directionButtons[key].style.visibility = 'visible';
+    }
+
 }
+
+function getinput(){
+    show(myinput.value);
+    //console.log(myinput.value)
+    myinput.value = "";
+    myinput.focus();
+}
+
+function goDirection(direction){
+    let punt_index = lokaties[current_index].directions[direction];
+    show(punt_index)
+}
+
+show(0)
